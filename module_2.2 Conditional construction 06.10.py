0df9b03a60c8e743dbb39ffa9d013920bb85bf08
AlexPrivalov78 <privalov_78@list.ru>
2024-10-14 08:37:28 +0300

    Add files via upload

---
+++ module_2.2 Conditional construction 06.10.py
@@ -0,0 +1,28 @@
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and first == third:
+    print(3)
+elif first == second or first == third:
+    print(2)
+else:
+    print(0)
+
+
+
+# name = input('Введите Ваше имя: ')
+# if name == 'Urban':
+#     print('Здравствуйте, администратор!')
+# if name == 'Денис':
+#     print('Здравствуйте, преподаватель!')
+# else:
+#     print("Привет, ", name,'!')
+# number = int(input("Введите число: "))   # Fizz, Buzz, FizzBuzz
+# if number % 3 == 0 and number % 5 == 0:
+#     print('FizzBuzz')
+# elif number % 3 == 0:
+#     print('Fizz')
+# elif number % 5 == 0:
+#     print('Buzz')
+# else:
+#     print('Число не подходит!')
