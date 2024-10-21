6e85127bef401ddf81a80b846d64b2e29484b1be
AlexPrivalov78 <privalov_78@list.ru>
2024-10-21 19:31:25 +0300

    Add files via upload

--- module_2.3 Code style Cycle While.py
+++ module_2.3 Code style Cycle While.py
@@ -3,51 +3,9 @@ index = 0
 while index < len(my_list):
     if my_list [index] == 0:
         continue
-    if my_list [index] <= 0:
+    if my_list [index] < 0:
         index += 1
         break
-    if my_list [index] >= 0:
+    if my_list [index] > 0:
         index += 1
     print(my_list [index])
-
-
-
-
-        # positive_numbers = []
-    # [i] = range(len(my_list))
-
-        # sorted()initial_value = 0
-        #     if positive_numbers > 0
-        #         = [i] > initial_value
-    #for i in range(len(my_list)):
-
-
-
-        #list_.remove(i)
-#    number =
-
-
-# dict_ = {'a' : 1, 'b' : 2, 'c' : 3}
-# for i, k in dict_.items():
-#     print(i, '=', k)
-# for i in range(1, 11):
-#     for j in range(1, 11):
-#         print(f'{i} x {j} = {i * j}')
-
-
-# list_ = ['one', 'two', 'three']
-# list_2 = [2, 3, 4, 5, 1]
-# sum_ = 0
-# for i in range(len(list_2)):
-#     sum_ += list_2[i]
-# print(sum_)
-# print(list_2)
-
-
-    #list_[i] = ' :( '
-    #if i == 'three':
-        #list_.remove(i)
-
-#print(list_)
-
-
